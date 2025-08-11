c771a8af28c687b7e7fb0667f232325bafa74ed6
Nichu055 <ulasiebuka2@gmail.com>
2025-08-11 18:05:13 +0100

    route admin login

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import AdminLogin from './Admin/Login';
 
 function App(): React.ReactElement {
   return (
@@ -9,6 +10,7 @@ function App(): React.ReactElement {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
         {/* Add more routes here */}
       </Routes>
     </BrowserRouter>
