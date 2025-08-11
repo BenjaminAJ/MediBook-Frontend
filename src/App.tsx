817a07fc0323643fb633a7cf6a005d906468ac65
Nichu055 <ulasiebuka2@gmail.com>
2025-08-11 16:27:56 +0100

    add routing for Login and Register pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App(): React.ReactElement {
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* Add more routes here */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
