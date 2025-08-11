bd368632b5295922f289bad40f0883bc8308edd3
Nichu055 <ulasiebuka2@gmail.com>
2025-08-11 18:33:33 +0100

    chnage router to createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,45 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import AdminLogin from './Admin/Login';
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  RouteObject
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import AdminLogin from "./Admin/Login";
+import AdminDashboard from "./Admin/Dashboard";
+import AdminUserManagement from "./Admin/UserManagement";
+import AdminAuditLogs from "./Admin/AuditLogs";
+
+// Admin dashboard layout with nested routes
+const adminDashboardRoutes: RouteObject = {
+  path: "/admin/dashboard",
+  element: <AdminDashboard />,
+  children: [
+    { path: "users", 
+      element: <AdminUserManagement />
+    },
+    { path: "audit-logs", 
+      element: <AdminAuditLogs /> 
+    },
+  ],
+};
+
+const router = createBrowserRouter([
+  { path: "/", 
+    element: <Login /> 
+  },
+  { path: "/register", 
+    element: <Register /> 
+  },
+  { path: "/admin/login", 
+    element: <AdminLogin /> 
+  },
+  adminDashboardRoutes,
+]);
 
 function App(): React.ReactElement {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/admin/login" element={<AdminLogin />} />
-        {/* Add more routes here */}
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
