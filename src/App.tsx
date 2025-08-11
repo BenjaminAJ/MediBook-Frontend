276a4bc58d2451f3e1d1119a5b16886bb7ce5c48
Nichu055 <ulasiebuka2@gmail.com>
2025-08-11 16:56:30 +0100

    update route

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): React.ReactElement {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         {/* Add more routes here */}
       </Routes>
